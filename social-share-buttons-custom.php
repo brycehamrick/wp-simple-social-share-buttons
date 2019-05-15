6f10d922cb1be990660cb50ae029ea4b5596b5b6
Kyle Stockmann <kyle@drbrighten.com>
2019-05-15 15:45:27 -0700

    added via

--- social-share-buttons-custom.php
+++ social-share-buttons-custom.php
@@ -54,7 +54,7 @@ Class Social_Share_Button_Custom {
 			// Get Twitter Handle from meta tag
 			$twitterHandle = "@".get_option('twitter-handle');
 			// Construct sharing URL without using any script
-			$twitterURL = 'https://twitter.com/intent/tweet?text='.$sharerTitle.'&amp;url='.$sharerURL.'&amp;text='.$twitterHandle;
+			$twitterURL = 'https://twitter.com/intent/tweet?text='.$sharerTitle.'&amp;url='.$sharerURL.'&amp;via='.$twitterHandle;
 			$facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$sharerURL;
 			$googleURL = 'https://plus.google.com/share?url='.$sharerURL;
 			$bufferURL = 'https://bufferapp.com/add?url='.$sharerURL.'&amp;text='.$sharerTitle;
